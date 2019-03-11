39aed4dfe3736a5ef7fd91a31e4e03cb36f17e46
Clément Bouillier <clement.bouillier@devcrafting.com>
2019-03-11 07:42:53 +0100

    Refactor Offer

--- csharp/Offer.cs
+++ csharp/Offer.cs
@@ -7,14 +7,14 @@ namespace supermarket
 
     public class Offer
     {
-        public SpecialOfferType OfferType { get; }
+        private readonly SpecialOfferType _offerType;
         private Product _product;
-        public double Argument { get; }
+        private readonly double _argument;
 
         public Offer(SpecialOfferType offerType, Product product, double argument)
         {
-            this.OfferType = offerType;
-            this.Argument = argument;
+            this._offerType = offerType;
+            this._argument = argument;
             this._product = product;
         }
 
@@ -22,40 +22,40 @@ namespace supermarket
         {
             var quantityAsInt = (int)quantity;
             var x = 1;
-            if (this.OfferType == SpecialOfferType.ThreeForTwo)
+            if (this._offerType == SpecialOfferType.ThreeForTwo)
             {
                 x = 3;
 
             }
-            else if (this.OfferType == SpecialOfferType.TwoForAmount)
+            else if (this._offerType == SpecialOfferType.TwoForAmount)
             {
                 x = 2;
                 if (quantityAsInt >= 2)
                 {
-                    double total = this.Argument * quantityAsInt / x + quantityAsInt % 2 * unitPrice;
+                    double total = this._argument * quantityAsInt / x + quantityAsInt % 2 * unitPrice;
                     double discountN = unitPrice * quantity - total;
-                    return new Discount(product, "2 for " + this.Argument, discountN);
+                    return new Discount(product, "2 for " + this._argument, discountN);
                 }
 
             }
-            if (this.OfferType == SpecialOfferType.FiveForAmount)
+            if (this._offerType == SpecialOfferType.FiveForAmount)
             {
                 x = 5;
             }
             var numberOfXs = quantityAsInt / x;
-            if (this.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+            if (this._offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
             {
                 var discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
                 return new Discount(product, "3 for 2", discountAmount);
             }
-            if (this.OfferType == SpecialOfferType.TenPercentDiscount)
+            if (this._offerType == SpecialOfferType.TenPercentDiscount)
             {
-                return new Discount(product, this.Argument + "% off", quantity * unitPrice * this.Argument / 100.0);
+                return new Discount(product, this._argument + "% off", quantity * unitPrice * this._argument / 100.0);
             }
-            if (this.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
+            if (this._offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
             {
-                var discountTotal = unitPrice * quantity - (this.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                return new Discount(product, x + " for " + this.Argument, discountTotal);
+                var discountTotal = unitPrice * quantity - (this._argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                return new Discount(product, x + " for " + this._argument, discountTotal);
             }
 
             return null;
