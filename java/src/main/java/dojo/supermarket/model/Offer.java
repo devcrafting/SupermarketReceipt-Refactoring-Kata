afb48dc27665b89c41da190645f15e775fb136b1
Clément Bouillier <clement.bouillier@devcrafting.com>
2019-07-16 18:42:23 +0200

    Move discount calculation to Offer (prepare for Offer polymorphism)

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -15,4 +15,41 @@ public class Offer {
         return this.product;
     }
 
+    public Discount getDiscount(double quantity, double unitPrice) {
+        int quantityAsInt = (int) quantity;
+        if (offerType == SpecialOfferType.TwoForAmount) {
+            return getXForAmountDiscount(2, quantity, argument, unitPrice, quantityAsInt);
+        } else if (offerType == SpecialOfferType.ThreeForTwo) {
+            return  getThreeForTwo(quantity, unitPrice, quantityAsInt);
+        } else if (offerType == SpecialOfferType.TenPercentDiscount) {
+            return new Discount(product, argument + "% off", quantity * unitPrice * argument / 100.0);
+        } else if (offerType == SpecialOfferType.FiveForAmount) {
+            return getXForAmountDiscount(5, quantity, argument, unitPrice, quantityAsInt);
+        } else {
+            return null;
+        }
+    }
+
+    private Discount getXForAmountDiscount(int nbPerPackage, double quantity, double packagePrice, double unitPrice, int quantityAsInt) {
+        if (quantityAsInt < nbPerPackage) {
+            return null;
+        }
+        int nbOfPackages = quantityAsInt / nbPerPackage;
+        double normalPrice = unitPrice * quantity;
+        int nbItemsOutOfPackage = quantityAsInt % nbPerPackage;
+        double discountedPrice =
+                packagePrice * nbOfPackages
+                        + nbItemsOutOfPackage * unitPrice;
+        double discountTotal = normalPrice - discountedPrice;
+        return new Discount(product, nbPerPackage + " for " + packagePrice, discountTotal);
+    }
+
+    private Discount getThreeForTwo(double quantity, double unitPrice, int quantityAsInt) {
+        if (quantityAsInt <= 2) {
+            return null;
+        }
+        int numberOfXs = quantityAsInt / 3;
+        double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+        return new Discount(product, "3 for 2", discountAmount);
+    }
 }
--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -39,43 +39,10 @@ public class ShoppingCart {
             if (offers.containsKey(p)) {
                 Offer offer = offers.get(p);
                 double unitPrice = catalog.getUnitPrice(p);
-                int quantityAsInt = (int) quantity;
-                Discount discount = null;
-                if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                    discount = getXForAmountDiscount(2, p, quantity, offer.argument, unitPrice, quantityAsInt);
-                } else if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                    discount = getThreeForTwo(p, quantity, unitPrice, quantityAsInt);
-                } else if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-                    discount = new Discount(p, offer.argument + "% off", quantity * unitPrice * offer.argument / 100.0);
-                } else if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                    discount = getXForAmountDiscount(5, p, quantity, offer.argument, unitPrice, quantityAsInt);
-                }
+                Discount discount = offer.getDiscount(quantity, unitPrice);
                 if (discount != null)
                     receipt.addDiscount(discount);
             }
         }
-    }
-
-    private Discount getXForAmountDiscount(int nbPerPackage, Product p, double quantity, double packagePrice, double unitPrice, int quantityAsInt) {
-        if (quantityAsInt < nbPerPackage) {
-            return null;
-        }
-        int nbOfPackages = quantityAsInt / nbPerPackage;
-        double normalPrice = unitPrice * quantity;
-        int nbItemsOutOfPackage = quantityAsInt % nbPerPackage;
-        double discountedPrice =
-                packagePrice * nbOfPackages
-                + nbItemsOutOfPackage * unitPrice;
-        double discountTotal = normalPrice - discountedPrice;
-        return new Discount(p, nbPerPackage + " for " + packagePrice, discountTotal);
-    }
-
-    private Discount getThreeForTwo(Product p, double quantity, double unitPrice, int quantityAsInt) {
-        if (quantityAsInt <= 2) {
-            return null;
-        }
-        int numberOfXs = quantityAsInt / 3;
-        double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
-        return new Discount(p, "3 for 2", discountAmount);
     }
 }
